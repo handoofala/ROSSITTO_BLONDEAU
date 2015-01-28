21decc6a2aa38f931dffe2b5cd1845735e1e75a0
handoofala <handoofala@gmail.com>
2015-01-28 16:36:36 +0100

    Edition of File class

--- Logger/src/File.java
+++ Logger/src/File.java
@@ -1,12 +1,24 @@
+import java.io.*;
 
-public class File {
+public class File extends Cibles{
 
 	public File(){
 			
 	}
 		
-	public void write(String message){
-		
+	public void write(String chaine){
+		String fichier ="Logger.properties";
+		//création ou ajout dans le fichier texte
+		try {
+			FileWriter fw = new FileWriter (fichier);
+			BufferedWriter bw = new BufferedWriter (fw);
+			PrintWriter fichierSortie = new PrintWriter (bw); 
+			fichierSortie.println (chaine); 
+			fichierSortie.close();
+		}
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
 	}
 
 }
