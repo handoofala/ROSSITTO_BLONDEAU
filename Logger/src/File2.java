f1a43156e36a8d82fbae50106ebe5f0eb16ccb27
handoofala <handoofala@gmail.com>
2015-02-26 15:43:04 +0100

    Starting to create rotative file (toto.log) with File2.java

--- Logger/src/File2.java
+++ Logger/src/File2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.io.File;
 
 
 public class File2 extends Cibles{
@@ -11,9 +12,21 @@ public class File2 extends Cibles{
 		
 	public void write(String chaine){
 		String fichier2 ="toto.log";
+		boolean continueWriting = true;
+		File file = new File(fichier2);
+		if(file.exists() && !file.isDirectory()){
+			//le fichier existe déjà. On vérifie donc sa taille. Si la taille est trop grande, on réecrira au début du fichier.
+			if(file.length() > 500){
+				System.out.println("Trop long! : "+file.length());
+				continueWriting = false;
+			}
+			else{
+				System.out.println("file exist! : "+file.length());
+			}
+		}
 		//création ou ajout dans le fichier texte
 		try {
-			FileWriter fw = new FileWriter (fichier2, true);
+			FileWriter fw = new FileWriter (fichier2, continueWriting);
 			BufferedWriter bw = new BufferedWriter (fw);
 			PrintWriter fichierSortie = new PrintWriter (bw); 
 			fichierSortie.println (chaine); 
